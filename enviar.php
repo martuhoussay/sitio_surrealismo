1b821444ede47e0fbab6eb4f8a2f00ecc10e122d
martuhoussay <martinahoussay@gmail.com>
2023-05-08 23:00:41 -0300

    Initial commit

---
+++ enviar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Enviar</title>
+</head>
+
+<body>
+<?php
+ 
+//se arma el array POST
+$nombre=$_POST['nombre'];
+$email=$_POST['email'];
+$localidad=$_POST['localidad'];
+$comentario=$_POST["comentario"];
+
+$destino="martinahoussay@gmail.com"; 
+$asunto="Contacto desde el sitio";
+$mensaje="Nombre: ".$nombre." Email: ".$email." Localidad: ".$localidad." Mensaje: ".$comentario;
+
+$header="From: ".$nombre."<".$email.">";
+
+$enviado = mail($destino,$asunto,$mensaje,$header);
+
+if($enviado == true){
+	echo "Su correo ha sido enviado.";
+}else{
+	echo "Hubo un error en el envio del mail.";
+}
+
+include('conexión.php');
+
+
+
+// $consulta=mysqli_query($conexion, "INSERT INTO contactos VALUES ('','$nombre','$email','$localidad','$comentario')");
+
+$consulta = mysqli_query($conexion, "INSERT INTO contactos (nombre,email,localidad,comentario)VALUES ( '$nombre','$email','$localidad','$comentario')") or die(mysqli_error($conexion));
+
+
+
+?>
+</body>
+</html>
